c7855e7959ddf66ce0f14daf916565a9b70667d5
oussema Mhamed <120645606+oussema445@users.noreply.github.com>
2024-10-04 01:59:50 +0200

    dsi33

--- src/main/java/com/oussema/pieces/entities/Pieces.java
+++ src/main/java/com/oussema/pieces/entities/Pieces.java
@@ -9,73 +9,92 @@ import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Pieces {
-	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)
-private Long idPiece ;
-private String nomPiece;
-private String model ;
-private Double prixPiece;
-private Date dateCreation;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPiece;
+    private String nomPiece;
+    private String model;
+    private Double prixPiece;
+    private Date dateCreation;
+    private String imageUrl; // New field for image URL
 
-@ManyToOne
-private Nature nature ;
+    @ManyToOne
+    private Nature nature;
 
+    // Default constructor
+    public Pieces() {
+        super();
+    }
 
-public Pieces() {
-	super();
-	// TODO Auto-generated constructor stub
-}
-public Long getIdPiece() {
-	return idPiece;
-}
-public void setIdPiece(Long idPiece) {
-	this.idPiece = idPiece;
-}
-public String getNomPiece() {
-	return nomPiece;
-}
-public void setNomPiece(String nomPiece) {
-	this.nomPiece = nomPiece;
-}
-public Double getPrixPiece() {
-	return prixPiece;
-}
-public void setPrixPiece(Double prixPiece) {
-	this.prixPiece = prixPiece;
-}
-public Date getDateCreation() {
-	return dateCreation;
-}
-public void setDateCreation(Date dateCreation) {
-	this.dateCreation = dateCreation;
-}
-public Pieces(String nomPiece, Double prixPiece, Date dateCreation,String model) {
-	super();
-	this.nomPiece = nomPiece;
-	this.prixPiece = prixPiece;
-	this.dateCreation = dateCreation;
-	this.model=model;
-}
-@Override
-public String toString() {
-	return "Pieces [idPiece=" + idPiece + ", nomPiece=" + nomPiece +", model"+ model + ", prixPiece=" + prixPiece + ", dateCreation="
-			+ dateCreation + "]";
-}
-public Nature getNature() {
-	return nature;
-}
-public void setNature(Nature nature) {
-	this.nature = nature;
-}
-public String getModel() {
-	return model;
-}
-public void setModel(String model) {
-	this.model = model;
-}
+    // Constructor
+    public Pieces(String nomPiece, Double prixPiece, Date dateCreation, String model, String imageUrl) {
+        super();
+        this.nomPiece = nomPiece;
+        this.prixPiece = prixPiece;
+        this.dateCreation = dateCreation;
+        this.model = model;
+        this.imageUrl = imageUrl;
+    }
 
+    // Getters and setters
+    public Long getIdPiece() {
+        return idPiece;
+    }
 
+    public void setIdPiece(Long idPiece) {
+        this.idPiece = idPiece;
+    }
 
+    public String getNomPiece() {
+        return nomPiece;
+    }
 
+    public void setNomPiece(String nomPiece) {
+        this.nomPiece = nomPiece;
+    }
 
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Double getPrixPiece() {
+        return prixPiece;
+    }
+
+    public void setPrixPiece(Double prixPiece) {
+        this.prixPiece = prixPiece;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public Nature getNature() {
+        return nature;
+    }
+
+    public void setNature(Nature nature) {
+        this.nature = nature;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Pieces [idPiece=" + idPiece + ", nomPiece=" + nomPiece + ", model=" + model + ", prixPiece=" + prixPiece + ", dateCreation=" + dateCreation + ", imageUrl=" + imageUrl + "]";
+    }
 }
